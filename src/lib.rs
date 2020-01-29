6661b0fbe3be42f350de004f2272619f2ce1cf22
Andres Medina <amedina@pivotal.io>
2020-01-29 13:48:51 -0800

    use new_spanned

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,12 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use syn::spanned::Spanned;
 
 #[proc_macro_attribute]
 pub fn method(_args: TokenStream, method: TokenStream) -> TokenStream {
     let method = syn::parse_macro_input!(method as syn::ImplItemMethod);
     let receiver = method.sig.inputs.first().unwrap();
-    syn::Error::new(receiver.span(), "this error should also cover the type")
+    syn::Error::new_spanned(receiver, "this error should also cover the type")
         .to_compile_error()
         .into()
 }
